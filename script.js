eba7c25b7ccfce92eddbd3820d879ddd2db90ba4
carsalcedo <fercarrera07@gmail.com>
2026-01-06 22:10:44 -0400

    terminada

--- script.js
+++ script.js
@@ -21,3 +21,90 @@ pageTurnBtn.forEach((el, index) => {
         }
     }
 })
+
+//contactme button action
+
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() =>{
+                page.style.zIndex = 20 + index;
+            }, 500)
+
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+//reverse inde function
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex(){
+    pageNumber--;
+    if(pageNumber < 0){
+        pageNumber = totalPages - 1;
+    }
+}
+
+//profile back button
+
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+//opening animation
+
+const coverRight = document.querySelector('.cover.cover-right')
+const pageLetf = document.querySelector('.book-page.page-left')
+
+//opening animation cover right animation
+
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+//opening animation page profile animation
+
+setTimeout(() => {
+    pageLetf.style.zIndex = 20;
+}, 3200)
+
+//opening animation all pages right animation
+
+ pages.forEach((_, index) => {
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+
+        }, (index + 1) * 200 + 2100)
+    })
